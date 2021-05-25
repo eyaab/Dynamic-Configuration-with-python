4afb3e40825e6676a551594b0de1f0a34ed55b6d
ghaida bouchaala <ghaida.bouchaala@gmail.com>
2021-05-25 20:13:50 +0100

    project structure updated

--- run.py
+++ run.py
@@ -1,7 +1,5 @@
-from flask import Flask, render_template
-app = Flask(__name__)
+from app import app
 
 
-@app.route('/')
-def hello_world():
-    return render_template("index.html")
+if __name__ == '__run__':
+	app.run(debug = True)
